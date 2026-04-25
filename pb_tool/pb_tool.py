0a7208c242d0bf42b32ced6fcf7e4d7be8290069
jonah <geosull@pm.me>
2026-04-25 11:13:40 +1000

    Exit with error code when deployment fails
    Call sys.exit(1) after reporting install errors
    
    Closes #30

--- pb_tool/pb_tool.py
+++ pb_tool/pb_tool.py
@@ -231,6 +231,7 @@ def install_files(plugin_dir, cfg):
             "UI and resource files are compiled. Using dclean to delete the\n"
             "plugin before deploying may also help."
         )
+        sys.exit(1)
 
 
 def clean_deployment(ask_first=True, config="pb_tool.cfg", plugin_dir=None):
